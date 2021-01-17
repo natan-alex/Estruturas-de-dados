6e85bdcff0a4144d4697a78e15c9c82f9547c8e8
natan-alex <natancarmo321@gmail.com>
2021-01-17 15:33:58 -0300

    mais opcoes em continuar()

--- java/Main.java
+++ java/Main.java
@@ -15,12 +15,16 @@ public class Main {
 		char opcao;
 		do {
 			System.out.println("Continuar?");
-			System.out.println("[s] - sim");
-			System.out.println("[n] - não");
+			System.out.println("[s ou 1] - sim");
+			System.out.println("[n ou 2] - não");
 			System.out.print("Opção: ");
 			opcao = input.next().charAt(0);
 			opcao = Character.toLowerCase(opcao);
-		} while (opcao != 's' && opcao != 'n');
+		} while (opcao != 's' && opcao != 'n' && opcao != '1' && opcao != '2');
+		if (opcao == '1')
+			opcao = 's';
+		else if (opcao == '2')
+			opcao = 'n';
 		return opcao;
 	}
 
